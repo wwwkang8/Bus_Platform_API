85dfe551b846806694c5fe98b9e3f40e650294ec
JeongHo <wwwkang8@gmail.com>
2019-04-16 21:20:03 +0900

    initial commit

---
+++ src/main/java/com/mobility/anybus/AnybusApplication.java
@@ -0,0 +1,13 @@
+package com.mobility.anybus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnybusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnybusApplication.class, args);
+	}
+
+}
